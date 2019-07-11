49ee727d3bc478447ce603b70091a0ce11196411
ragavanrocky <52520783+ragavanrocky@users.noreply.github.com>
2019-07-11 17:35:47 +0530

    Update four.c

--- four.c
+++ four.c
@@ -1,3 +1,10 @@
 #include<stdio.h>
 void main()
 {
+  char c;
+  scanf("%c",&c);
+  if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='b'||c=='c'||c=='d'||c=='f'||c=='g'||c=='h'||c=='j'||c=='k'||c=='l'||c=='m'||c=='n'||c=='p'||c=='q'||c=='r'||c=='s'||c=='t'||c=='v'||c=='w'||c=='x'||c=='y'||c=='z')
+    printf("Alphabet");
+  else
+    printf("No");
+}
